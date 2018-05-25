96e80118f19bd9c5bfc49ea3b8edc5abf71231f5
ha1f <ha1f@linecorp.com>
2018-05-25 17:20:27 +0900

    Initial Commit

---
+++ NeuralNetSample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NeuralNetSample
+//
+//  Created by ST20591 on 2018/05/25.
+//  Copyright © 2018年 ha1f. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
