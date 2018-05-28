f3ac8fdf91c783a33821fa488e91aba5b7e30c1b
ha1f <ha1f@linecorp.com>
2018-05-29 02:03:44 +0900

    Add functions

--- NeuralNetSample/Matrix.swift
+++ NeuralNetSample/Matrix.swift
@@ -11,18 +11,11 @@ import Accelerate
 
 /// 縦にrow、横にcolumn
 /// http://yamaimo.hatenablog.jp/entry/2016/03/28/200000
+/// la_retain, la_releaseは実装してないので謎
 struct Matrix {
     let raw: la_object_t
     
-    var rows: la_count_t {
-        return la_matrix_rows(raw)
-    }
-    
-    var cols: la_count_t {
-        return la_matrix_cols(raw)
-    }
-    
-    /// 対角行列とかのときはヒントを指定するとはやくなる
+    /// 対角行列とかのときはヒントを指定するとはやくなるかも
     init?(array: [Double], rows: la_count_t, cols: la_count_t, hint: la_hint_t = la_hint_t(LA_NO_HINT)) {
         guard array.count == rows * cols else {
             debugPrint("array.count(\(array.count) does not matches rows * cols(\(rows * cols)")
@@ -39,6 +32,22 @@ struct Matrix {
         }
         self.raw = matrix
     }
+}
+
+extension Matrix {
+    // MARK: - Getter
+    
+    var rows: la_count_t {
+        return la_matrix_rows(raw)
+    }
+    
+    var cols: la_count_t {
+        return la_matrix_cols(raw)
+    }
+    
+    var status: la_status_t {
+        return la_status(raw)
+    }
     
     func getComponents() -> [Double] {
         let rows = self.rows
@@ -49,6 +58,8 @@ struct Matrix {
 }
 
 extension Matrix {
+    // MARK: - Static Builder
+    
     static func vector(_ array: [Double]) -> Matrix {
         return Matrix(array: array, rows: la_count_t(array.count), cols: 1)!
     }
@@ -61,22 +72,108 @@ extension Matrix {
         let vector = Matrix.vector(array)
         return Matrix(la_diagonal_matrix_from_vector(vector.raw, diagonal))!
     }
+    
+    /// splatはサイズを持たないが、全ての要素がvalueとみなされる行列
+    static func splat(fromValue value: Double) -> Matrix {
+        return Matrix(la_splat_from_double(value, la_attribute_t(LA_DEFAULT_ATTRIBUTES)))!
+    }
+    
+    /// splatをベクトルの要素から生成
+    static func splat(fromElementOf vector: Matrix, index: la_index_t) -> Matrix? {
+        return Matrix(la_splat_from_vector_element(vector.raw, index))
+    }
+    
+    /// splatをmatrixの要素から生成
+    static func splat(fromElementOf matrix: Matrix, row: la_index_t, col: la_index_t) -> Matrix? {
+        return Matrix(la_splat_from_matrix_element(matrix.raw, row, col))
+    }
 }
 
 extension Matrix {
+    // MARK: - Static Operator
+    
+    /// サイズが同じである必要がある
+    static func sum(_ lhs: Matrix, _ rhs: Matrix) -> Matrix? {
+        return Matrix(la_sum(lhs.raw, rhs.raw))
+    }
+    
+    /// サイズが同じである必要がある
+    static func difference(_ lhs: Matrix, _ rhs: Matrix) -> Matrix? {
+        return Matrix(la_difference(lhs.raw, rhs.raw))
+    }
+    
+    /// 積
+    static func product(_ lhs: Matrix, _ rhs: Matrix) -> Matrix? {
+        return Matrix(la_matrix_product(lhs.raw, rhs.raw))
+    }
+    
+    /// アダマール積
+    /// サイズが同じである必要がある
+    static func elementwiseProduct(_ lhs: Matrix, _ rhs: Matrix) -> Matrix? {
+        return Matrix(la_elementwise_product(lhs.raw, rhs.raw))
+    }
+    
+    // solve AX=B
+    static func solve(_ system: Matrix, _ rhs: Matrix) -> Matrix? {
+        return Matrix(la_solve(system.raw, rhs.raw))
+    }
+}
+
+extension Matrix {
+    // MARK: - Slicing
+    
     func sliced(firstRow: la_index_t, firstCol: la_index_t, sliceRows: la_count_t, sliceCols: la_count_t, rowStride: la_index_t = 1, colStride: la_index_t = 1) -> Matrix? {
         return Matrix(la_matrix_slice(raw, firstRow, firstCol, rowStride, colStride, sliceRows, sliceCols))
     }
     
-    func vector(row: la_count_t) -> Matrix? {
+    func vectorFromRow(_ row: la_count_t) -> Matrix? {
         return Matrix(la_vector_from_matrix_row(raw, row))
     }
     
-    func vector(col: la_count_t) -> Matrix? {
+    func vectorFromCol(_ col: la_count_t) -> Matrix? {
         return Matrix(la_vector_from_matrix_col(raw, col))
     }
     
-    func diagonalVector(_ diagonal: la_index_t = 0) -> Matrix? {
+    func vectorFromDiagonal(_ diagonal: la_index_t = 0) -> Matrix? {
         return Matrix(la_vector_from_matrix_diagonal(raw, diagonal))
     }
+}
+
+extension Matrix {
+    // MARK: - Transforming
+    
+    func transposed() -> Matrix? {
+        return Matrix(la_transpose(raw))
+    }
+    
+    func scaled(by scale: Double) -> Matrix? {
+        return Matrix(la_scale_with_double(raw, scale))
+    }
+}
+
+extension Matrix {
+    // MARK: - For vectors
+    
+    var length: la_count_t {
+        return la_vector_length(raw)
+    }
+    
+    /// 内積
+    static func innerProduct(_ lhs: Matrix, _ rhs: Matrix) -> Double? {
+        return Matrix(la_inner_product(lhs.raw, rhs.raw))?.getComponents().first
+    }
+    
+    /// 外積
+    static func outerProduct(_ lhs: Matrix, _ rhs: Matrix) -> Matrix? {
+        return Matrix(la_outer_product(lhs.raw, rhs.raw))
+    }
+    
+    func norm(_ norm: la_norm_t = la_norm_t(LA_L1_NORM)) -> Double? {
+        return la_norm_as_double(raw, norm)
+    }
+    
+    func normalized(_ norm: la_norm_t = la_norm_t(LA_L1_NORM)) -> Matrix? {
+        return Matrix(la_normalized_vector(raw, norm))
+    }
+    
 }
