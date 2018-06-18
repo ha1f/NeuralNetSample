e7369578f59ef20c0587afbf746749e840574cf0
ha1f <ha1f@linecorp.com>
2018-06-18 16:15:42 +0900

    used accelerate for sigmoid

--- NeuralNetSample/Common/Layer/SigmoidLayer.swift
+++ NeuralNetSample/Common/Layer/SigmoidLayer.swift
@@ -7,9 +7,31 @@
 //
 
 import Foundation
+import Accelerate
 
 class SigmoidLayer: LayerType {
     private var _outputCache: [Double]?
+    
+    func forward(batchInput input: Matrix) -> Matrix {
+        let components = input.getComponents()
+        
+        let length = components.count
+        var results = [Double](repeating: 0, count: length)
+        let ones = [Double](repeating: 1, count: length)
+        
+        components.withUnsafeBufferPointer { pointer0 in
+            results.withUnsafeMutableBufferPointer { resultPointer in
+                cblas_dcopy(Int32(length), pointer0.baseAddress!, 1, resultPointer.baseAddress!, 1)
+                cblas_dscal(Int32(length), -1, resultPointer.baseAddress!, 1)
+                vvexp(resultPointer.baseAddress!, resultPointer.baseAddress!, [numericCast(length)])
+                vDSP_vaddD(resultPointer.baseAddress!, 1, ones, 1, resultPointer.baseAddress!, 1, vDSP_Length(length))
+                vvrec(resultPointer.baseAddress!, resultPointer.baseAddress!, [numericCast(length)])
+            }
+        }
+        
+        return Matrix(array: results, rows: input.rows, cols: input.cols)!
+        // return Matrix(array: components.map { 1 / (1 + exp(-$0)) }, rows: input.rows, cols: input.cols)!
+    }
     
     func forward(_ input: [Double]) -> [Double] {
         let result = input.map { 1 / (1 + exp(-$0)) }
